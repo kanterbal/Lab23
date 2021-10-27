66afb8ace67d99d83f8cb14ecceb5ce89545ebf0
Sergei Kanter <sdkanter@outlook.com>
2021-10-27 18:29:29 +0200

    ex done.

---
+++ Lab23/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int res = FactorialAsync(n).Result;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+
+        static async Task<int> FactorialAsync(int n)
+        {
+            int res = await Task.Run(() => Factorial(n));
+            return res;
+        }
+
+    }
+}
